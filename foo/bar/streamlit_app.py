98ebab0bc8e482d0701d1012f64096c7e8f87376
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 15:19:02 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -14,9 +14,13 @@ df = pd.read_csv("https://raw.githubusercontent.com/fallenpetal1/DATA200/main/fo
 # Extract only rows with Illness TRUE
 rows_with_illness_true = df.loc[df['Illness'] == 'Yes']
 # Plot showing ill people across cities
+
+
+
 fig = rows_with_illness_true.City.value_counts().plot.bar()
 a = np.random.normal(1, 1, size=50)
 fig,x = plt.subplots()
+plt.xticks(rotation=45)
 x.hist(a, bins=10)
 st.pyplot(fig, clear_figure=None)
 st.write("Test")
