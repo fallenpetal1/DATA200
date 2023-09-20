1c16743a9cacac3c0598f7428ebf5bb551a310c3
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 15:36:06 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -12,9 +12,8 @@ st.write(pd.DataFrame({
 
 df = pd.read_csv("https://raw.githubusercontent.com/fallenpetal1/DATA200/main/foo/bar/toy_dataset.csv")
 # df.hist(column = 'Income')
-
+fig, x = plt.subplots()
 gen_med = df.groupby(['City','Gender'])['Income'].mean().reset_index(name='count')
-print(gen_med)
 # creating the bar plot
 plt.bar(gen_med['City'], gen_med['count'])#,width = 0.4)
 
@@ -23,4 +22,4 @@ plt.ylabel("Average Income")
 plt.title("City")
 plt.xticks(rotation=45,horizontalalignment='right' )
 #plt.xlabel('x_description', , x=1.0)
-st.pyplot(plt)
+st.pyplot(fig)
