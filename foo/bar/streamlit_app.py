e436e430f8042684e1115bf0b15d7c66b7e47ea8
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 12:57:58 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+df = pd.read_csv("toy_dataset.csv")
+# Plot showing ill people across cities
+st.write(rows_with_illness_true.City.value_counts().plot.bar())
