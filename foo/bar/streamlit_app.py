23564617a3822e967871fc00dd7547420b320116
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 14:27:38 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import plotly.express as px 
 
 st.write("Here's our first attempt at using data to create a table:")
