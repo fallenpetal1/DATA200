3eefd2cb1e5ea708371c67bf45c94687813b0b67
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 14:13:08 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
@@ -8,7 +8,7 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 }))
 
-df = pd.read_csv("toy_dataset.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/fallenpetal1/DATA200/main/foo/bar/toy_dataset.csv")
 # Extract only rows with Illness TRUE
 rows_with_illness_true = df.loc[df['Illness'] == 'Yes']
 # Plot showing ill people across cities
