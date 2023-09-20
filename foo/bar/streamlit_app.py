110617c9fe76d687d43d874950600cf214600927
fallenpetal1 <82851140+fallenpetal1@users.noreply.github.com>
2023-09-20 15:11:03 -0700

    Update streamlit_app.py

--- foo/bar/streamlit_app.py
+++ foo/bar/streamlit_app.py
@@ -16,8 +16,8 @@ rows_with_illness_true = df.loc[df['Illness'] == 'Yes']
 # Plot showing ill people across cities
 fig = rows_with_illness_true.City.value_counts().plot.bar()
 a = np.random.normal(1, 1, size=50)
-fig = plt.subplots()
-fig.hist(a, bins=10)
+fig,x = plt.subplots()
+x.hist(a, bins=10)
 st.pyplot(fig, clear_figure=None)
 st.write("Test")
 # fig = px.density_heatmap(
